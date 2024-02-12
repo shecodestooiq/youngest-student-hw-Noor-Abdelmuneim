74306c35d14e64bae692437350bb291691909aae
Noor-Abdelmuneim <157888524+Noor-Abdelmuneim@users.noreply.github.com>
2024-02-12 17:16:48 +0300

    the task has been solved

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-function showYoungestStudent(students) { }
+function showYoungestStudent(personsArr, objProperty) {
 
-module.exports = showYoungestStudent;
+    let maxAge = 0;
+
+    for (let i = 0; i < personsArr.length; i++) {
+
+        if (personsArr[i][objProperty] > maxAge) {
+
+            maxAge = personsArr[i][objProperty];
+
+        }
+
+    }
+
+    return maxAge
+
+}
+const students = [
+    { name: 'Yasser', age: 18 },
+    { name: 'Ahmed', age: 27 },
+    { name: 'Noor', age: 23 },
+    { name: 'Sara', age: 25 },
+    { name: 'Hajer', age: 20 },
+];
+
+console.log(showYoungestStudent(students, "age"))
